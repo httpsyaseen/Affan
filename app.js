acc6c1a5b03fdbcf2febc6fb7afb9c0f374bfa37
httpsyaseen <yaseenwalker1@gmail.com>
2024-05-29 19:05:10 +0500

    test

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const postRoutes = require("./routes/posts");
+const path = require("path");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join("./views"));
+
+const CONNECTION_URL = "mongodb://localhost:27017/posts";
+const PORT = 5000;
+
+app.get("/", async (req, res) => {
+  res.render("index");
+});
+
+app.get("/new", async (req, res) => {
+  res.render("form");
+});
+
+mongoose
+  .connect("mongodb://0.0.0.0:27017/")
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.error(error.message));
+
+app.use("/posts", postRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
